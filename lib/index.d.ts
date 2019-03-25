7c35c9ee4636cd3645e615b98442e66ef0f79408
Kazuya Takei <attakei@gmail.com>
2019-03-25 21:54:14 +0900

    Build js from ts

---
+++ lib/index.d.ts
@@ -0,0 +1,22 @@
+/// <reference types="w3c-web-usb" />
+declare class CardInfo {
+    readonly spec: string;
+    readonly idm: string;
+    readonly pmm: string | null;
+    constructor(spec: string, idm: string, pmm: string | null);
+}
+declare class NFCDevice {
+    readonly device: USBDevice;
+    constructor(device: USBDevice);
+    receive(len: number): Promise<number[]>;
+    send(data: number[]): Promise<void>;
+    sendCommand(cmd: number, params: number[]): Promise<number[]>;
+    getType2TagInfo(): Promise<CardInfo | null>;
+    getType3TagInfo(): Promise<CardInfo>;
+    readCardInfo(): Promise<CardInfo | null>;
+    readIDm(): Promise<string>;
+}
+export declare class DeviceLoader {
+    static connectDevice(): Promise<NFCDevice>;
+}
+export {};
---
+++ lib/utils.d.ts
@@ -0,0 +1,1 @@
+export declare function arrayToHexString(arr: number[]): string;
